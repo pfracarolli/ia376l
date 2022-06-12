bd09c26d4a39fc8cd409d2c66706676b41321c43
Pedro Fracarolli <pfracarolli@gmail.com>
2022-06-12 19:53:13 -0300

    Rename `src` to `ia376l`

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='ia376l',
     packages=find_packages(),
     version='0.1.0',
-    description='Final project for Unicamp's IA376-L Deep Learning for Signal Synthesis course.',
+    description="Final project for Unicamp's IA376-L Deep Learning for Signal Synthesis course.",
     author='pfracarolli',
     license='',
 )
